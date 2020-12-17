1cc0461f462a5f2bb8ea76959c1ce74e4ba5d64a
junwo383 <75435635+junwo383@users.noreply.github.com>
2020-12-18 00:09:58 +0900

    Add files via upload

---
+++ 구구단.py
@@ -0,0 +1,9 @@
+
+print("|================구구단=======================|")
+
+while True :
+    guign = int(input("숫자를입력하세요!:"))
+    for i in range(1,10):
+        print(guign,"x",i,"=",guign*i)
+    else:
+        print("고객님!구구단을 이용해주셔서 감사합니다.!")
